7d2d2231a10fdf128f95aa7487a89e614e4092d6
Ryota Seki <ryotabs@hawaii.edu>
2020-05-18 20:16:19 -1000

    Change color scheme

--- config.h
+++ config.h
@@ -86,42 +86,48 @@ unsigned int tabspaces = 4;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+  /* 8 normal colors */
+  [0] = "#1c2023", /* black   */
+  [1] = "#c7ae95", /* red     */
+  [2] = "#95c7ae", /* green   */
+  [3] = "#31e9f1", /* yellow  */
+  [4] = "#ae95c7", /* blue    */
+  [5] = "#c795ae", /* magenta */
+  [6] = "#95aec7", /* cyan    */
+  [7] = "#c7ccd1", /* white   */
 
-	[255] = 0,
+  /* 8 bright colors */
+  [8]  = "#696969", /* black   */
+  [9]  = "#c7ae95", /* red     */
+  [10] = "#00ff80", /* green   */
+  [11] = "#90b56b", /* yellow  */
+  [12] = "#ae95c7", /* blue    */
+  [13] = "#c795ae", /* magenta */
+  [14] = "#00f0ff", /* cyan    */
+  [15] = "#ffffff", /* white   */
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
+  /* special colors */
+  [256] = "#1c2023", /* background */
+  [257] = "#c7ccd1", /* foreground */
 };
-
 
 /*
  * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
+ * foreground, background, cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 257;
+
+/*
+ * Colors used, when the specific fg == defaultfg. So in reverse mode this
+ * will reverse too. Another logic would only make the simple feature too
+ * complex.
+ */
+static unsigned int defaultitalic = 7;
+static unsigned int defaultunderline = 7;
 
 /*
  * Default shape of cursor
